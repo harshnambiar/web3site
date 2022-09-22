4a2e767006384fffe51a508ba526e68c89d215b9
harshnambiar <harrisonlvl2@gmail.com>
2022-09-22 20:57:04 +0530

    Initial commit.

---
+++ src/web3site_assets/src/index.js
@@ -0,0 +1,19 @@
+import { web3site } from "../../declarations/web3site";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await web3site.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
